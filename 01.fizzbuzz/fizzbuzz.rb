eb18bfc30b9fa7fbf3345d77670d907530029ec2
YukiWatanabe824 <gyggg950@gmail.com>
2021-03-23 21:28:49 +0900

    インデントをタブからスペース2個に修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 
 def fizzbuzz
-	(1..20).each {|num|
-		if (num % 3)== 0 && (num % 5)== 0
-			puts "FizzBuzz"
-		elsif (num % 5)== 0
-			puts "Buzz"
-		elsif (num % 3)== 0
-			puts "Fizz"
-		else
-			puts num
-		end
+  (1..20).each {|num|
+    if (num % 3)== 0 && (num % 5)== 0
+      puts "FizzBuzz"
+    elsif (num % 5)== 0
+      puts "Buzz"
+    elsif (num % 3)== 0
+      puts "Fizz"
+    else
+      puts num
+    end
 	}
 end
 
