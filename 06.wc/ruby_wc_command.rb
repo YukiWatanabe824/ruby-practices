d165252a5600e8f0c66edc4f5fcae3608db24319
YukiWatanabe824 <gyggg950@gmail.com>
2021-10-08 22:53:59 +0900

    コードレビューを踏まえてwcコマンドのコードを全体的に修正

--- 06.wc/ruby_wc_command.rb
+++ 06.wc/ruby_wc_command.rb
@@ -3,95 +3,74 @@
 
 require 'optparse'
 
-def count_lines(file)
-  File.open(file) do |io|
-    io.readlines.size
+def main
+  option = ARGV.getopts('l')
+  text_params = (FileTest.exist?(ARGV[0].to_s) ? generate_path_text : serch_pipe_input)
+  option['l'] ? short_format(text_params) : long_format(text_params)
+end
+
+def generate_path_text
+  ARGV.map do |path|
+    path_text = IO.read(path)
+    create_text_params(path_text)
   end
 end
 
-def count_words(text)
-  text.split(/\s+/).size
+def serch_pipe_input
+  !$stdin.tty? ? pipe_text : input_text
 end
 
-def count_bytesize(text)
-  text.split("\n").join.bytesize
+def pipe_text
+  pipe_text = $stdin.read
+  text_params = []
+  text_params << create_text_params(pipe_text)
 end
 
-def normal_wc
-  total_lines = 0
-  total_words = 0
-  total_bytesizes = 0
-  ARGV.each do |file|
-    text = File.read(file)
-    line = count_lines(file)
-    word = count_words(text)
-    bytesize = (count_bytesize(text) + ("\n".bytesize * line))
-
-    print line.to_s.rjust(8)
-    print word.to_s.rjust(8)
-    print bytesize.to_s.rjust(8)
-    puts " #{file}"
-
-    total_lines += line
-    total_words += word
-    total_bytesizes += bytesize
+def input_text
+  stdin_text = []
+  while (line = $stdin.gets)
+    stdin_text << line
   end
-  total_output(total_lines, total_words, total_bytesizes) if ARGV.size >= 2
+  text_params = []
+  text_params << create_text_params(stdin_text.join)
 end
 
-def total_output(total_lines, total_words, total_bytesizes)
-  print total_lines.to_s.rjust(8)
-  print total_words.to_s.rjust(8)
-  print total_bytesizes.to_s.rjust(8)
+def create_text_params(text)
+  {
+    line: text.split("\n").size,
+    word: text.split(/\s+/).size,
+    bytesize: text.bytesize
+  }
+end
+
+def short_format(text_params)
+  text_params.each_with_index do |params, idx|
+    print params[:line].to_s.rjust(8)
+    puts " #{ARGV[idx]}" if ARGV.size >= 0
+  end
+  line_total = text_params.map { |params| params[:line] }.sum
+  puts "#{line_total.to_s.rjust(8)} total" if text_params.size > 1
+end
+
+def long_format(text_params)
+  text_params.each_with_index do |params, idx|
+    print params[:line].to_s.rjust(8)
+    print params[:word].to_s.rjust(8)
+    print params[:bytesize].to_s.rjust(8)
+    puts " #{ARGV[idx]}" if ARGV.size >= 0
+  end
+  long_format_total(text_params) if text_params.size > 1
+end
+
+def long_format_total(text_params)
+  line_total = text_params.map { |params| params[:line] }.sum
+  word_total = text_params.map { |params| params[:word] }.sum
+  bytesize_total = text_params.map { |params| params[:bytesize] }.sum
+
+  print line_total.to_s.rjust(8)
+  print word_total.to_s.rjust(8)
+  print bytesize_total.to_s.rjust(8)
   puts ' total'
 end
 
-def save_stdin
-  attrs = []
-  while (line = $stdin.gets)
-    attrs << line.chomp
-  end
-  attrs
-end
-
-opt = OptionParser.new
-params = {}
-
-opt.on('-l') { |l| params[:l_option] = l }
-opt.parse!(ARGV)
-
-if params[:l_option] == true && FileTest.exist?(ARGV[0].to_s)
-  total_lines = 0
-  ARGV.each do |file|
-    line = count_lines(file)
-    print line.to_s.rjust(8)
-    puts " #{file}"
-
-    total_lines += line
-  end
-  if ARGV.size >= 2
-    print total_lines.to_s.rjust(8)
-    puts ' total'
-  end
-elsif params[:l_option] == true && !$stdin.tty?
-  io = $stdin.read
-  print io.count("\n").to_s.rjust(8)
-  puts
-elsif params[:l_option] == true
-  stdin_params = save_stdin
-  puts stdin_params.size.to_s.rjust(8)
-elsif FileTest.exist?(ARGV[0].to_s)
-  normal_wc
-elsif !$stdin.tty?
-  io = $stdin.read
-  bytesize = (count_bytesize(io) + ("\n".bytesize * io.count("\n")))
-  print io.count("\n").to_s.rjust(8)
-  print count_words(io).to_s.rjust(8)
-  print bytesize.to_s.rjust(8)
-  puts
-else
-  stdin_params = save_stdin
-  print stdin_params.size.to_s.rjust(8)
-  print stdin_params.join(' ').split(' ').size.to_s.rjust(8)
-  puts (stdin_params.join.bytesize + ("\n".bytesize * stdin_params.size)).to_s.rjust(8)
-end
+main
