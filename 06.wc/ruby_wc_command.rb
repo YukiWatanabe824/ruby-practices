fcfd6253705364615e4baf1aab10e4546b46745b
YukiWatanabe824 <gyggg950@gmail.com>
2021-10-12 12:37:01 +0900

    ファイル名をハッシュに格納する形に修正

--- 06.wc/ruby_wc_command.rb
+++ 06.wc/ruby_wc_command.rb
@@ -5,25 +5,31 @@ require 'optparse'
 
 def main
   option = ARGV.getopts('l')
+  #ARGVは可能な限り使わない。mainメソッドで変数に代入する
+  #それによって他所で使っているARGVを修正する
   text_params = (FileTest.exist?(ARGV[0].to_s) ? generate_path_text : serch_pipe_input)
+  # paramsはNG、〜listがよさげ
+  # search_pipe_inputはgenerate〜に揃える
   option['l'] ? short_format(text_params) : long_format(text_params)
+  #アウトプットするメソッドは統合してメソッド引数としてLオプションを取るほうがスマート
 end
 
 def generate_path_text
   ARGV.map do |path|
     path_text = IO.read(path)
-    create_text_params(path_text)
+    create_text_params(path_text, path)
   end
 end
 
 def serch_pipe_input
-  !$stdin.tty? ? pipe_text : input_text
+  $stdin.tty? ? input_text : pipe_text
 end
 
 def pipe_text
   pipe_text = $stdin.read
   text_params = []
-  text_params << create_text_params(pipe_text)
+  text_params << create_text_params(pipe_text, nil)
+  #【】を使って配列化する
 end
 
 def input_text
@@ -32,17 +38,25 @@ def input_text
     stdin_text << line
   end
   text_params = []
-  text_params << create_text_params(stdin_text.join)
+  text_params << create_text_params(stdin_text.join, nil)
+  #【】を使って配列化する
 end
 
-def create_text_params(text)
+def create_text_params(text, path)
   {
     line: text.split("\n").size,
     word: text.split(/\s+/).size,
-    bytesize: text.bytesize
+    bytesize: text.bytesize,
+    filename: (path == true ? path : nil)
   }
 end
 
+def filename
+
+end
+
+#出力の文字数を調整するメソッドを追加
+#出力メソッドは統合する
 def short_format(text_params)
   text_params.each_with_index do |params, idx|
     print params[:line].to_s.rjust(8)
