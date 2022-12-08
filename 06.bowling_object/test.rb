e47014df2b773ab88bd33410b7d9ed32a44fd7c1
YukiWatanabe824 <gyggg950@gmail.com>
2022-12-08 15:28:50 +0900

    test: frame 2投(0，0)のショットスコア合計値を返す

--- 06.bowling_object/test.rb
+++ 06.bowling_object/test.rb
@@ -5,20 +5,49 @@ class HelloTest < Minitest::Test
   def test_greeting
     assert_equal 'hello world', greeting
   end
+  describe 'Shot' do
+    def test_数値1を受け取って数値1を返す
+      shot = Shot.new(1)
+      assert_equal 1, shot.score
+    end
 
-  def test_数値1を受け取って数値1を返す
-    shot = Shot.new(1)
-    assert_equal 1, shot.score
+    def test_文字列Xを受け取って数値10を返す
+      shot = Shot.new('X')
+      assert_equal 10, shot.score
+    end
   end
 
-  def test_文字列Xを受け取って数値10を返す
-    shot = Shot.new('X')
-    assert_equal 10, shot.score
+  describe 'Frame' do
+    def test_0と0の2投の結果を受け取ってスコアの合計値を返す
+      frame = Frame.new(0, 0)
+      assert_equal 0, frame.score
+    end
   end
 end
 
 def greeting
   'hello world'
+end
+
+
+class Frame
+  attr_reader :first_shot, :second_shot, :third_shot
+
+#  - 1フレーム原則2投、例外で3投
+#    - 1〜3投の結果を受け取って、各ショットスコアの合計値で返す
+#  - 1投目で10本倒したらストライク
+#    - 1投目で10本倒したらストライクと判定する
+#  - 1投目で10本倒せず、2投目で全て倒したらスペア
+#    - 2投目で全て倒したらスペアと判定する
+  def initialize(first_mark, second_mark, third_mark =nil)
+    @first_shot = Shot.new(first_mark)
+    @second_shot = Shot.new(second_mark)
+    @third_shot = Shot.new(third_mark)
+  end
+
+  def score
+    @first_shot.score + @second_shot.score + @third_shot.score
+  end
 end
 
 class Shot
