c7aa19c6d78ce76ddb06feb4aa8ee2120b6aa0fd
YukiWatanabe824 <gyggg950@gmail.com>
2022-12-08 13:45:50 +0900

    test: shot 1を受け取って1を返す

--- 06.bowling_object/test.rb
+++ 06.bowling_object/test.rb
@@ -5,8 +5,32 @@ class HelloTest < Minitest::Test
   def test_greeting
     assert_equal 'hello world', greeting
   end
+
+  def test_数値1を受け取って数値1を返す
+    shot = Shot.new(1)
+    result = shot.score
+    assert_equal 1, result
+  end
+
+  def test_文字列Xを受け取って数値10を返す
+
+  end
 end
 
 def greeting
   'hello world'
+end
+
+class Shot
+  attr_reader :mark
+
+  def initialize(mark)
+    @mark = mark
+  end
+
+  def score
+    return 10 if mark =='X'
+
+    mark.to_i
+  end
 end
