9d9aa3b3abb121ba07443be8b1b4c5e7b212c7fc
YukiWatanabe824 <gyggg950@gmail.com>
2022-12-20 06:40:22 +0900

    refactor: 不要なコードの削除 & 最終フレームでのスペア処理修正

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -2,14 +2,24 @@
 
 require './frame'
 require './shot'
+require 'byebug'
 
 class Game
   FRAME_RANGE = 0..9
-  attr_accessor :score
 
   def initialize(score)
     @shots = score[0].split(',')
   end
+
+  def generate_score
+    game = generate_frames
+    game_score = 0
+
+    FRAME_RANGE.each { |n| game_score += cal_frame_score(game, n).score }
+    game_score
+  end
+
+  private
 
   def generate_frame(shot, frame, frames)
     if frames.size < 10
@@ -38,17 +48,11 @@ class Game
     left_shots = next_frame + after_next_frame
 
     return Frame.new(frame[0], frame[1], frame[2]) if num == 9 && frame[0] == 'X' # strike & last
+    return Frame.new(frame[0], frame[1], frame[2]) if num == 9 && frame[0].to_i + frame[1].to_i == 10 # spare & last
     return Frame.new(frame[0], left_shots[0], left_shots[1]) if frame[0] == 'X' # strike
     return Frame.new(frame[0], frame[1], left_shots[0]) if frame.map(&:to_i).sum == 10 # spare
 
     Frame.new(frame[0], frame[1])
   end
 
-  def generate_score
-    game = generate_frames
-    game_score = 0
-
-    FRAME_RANGE.each { |n| game_score += cal_frame_score(game, n).score }
-    game_score
-  end
 end
