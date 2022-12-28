b5b6568ef0007403594883ca86441a3c3c5b6f77
YukiWatanabe824 <gyggg950@gmail.com>
2022-12-29 00:46:24 +0900

    refactor: Gameクラスでのframeを構成する処理を削減

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -1,62 +1,16 @@
 # frozen_string_literal: true
 
 require './shot'
+require 'byebug'
 
 class Frame
-  FRAME_RANGE = 0..9
+  attr_reader :frame
+
   def initialize(shots)
-    @shots = shots
+    @frame = shots
   end
 
-  def generate_point_cal_frame
-    frames = generate_frames
-    point_cal_frame = []
-
-    FRAME_RANGE.each { |n| point_cal_frame << generate_cal_frame(frames, n) }
-
-    point_cal_frame
-  end
-
-  private
-
-  def generate_frames
-    frame = []
-    frames = []
-    @shots.each do |shot|
-      frame << shot
-      frames = generate_frame(shot, frame, frames)
-      frame.clear if frame.size >= 2 || shot == 10
-    end
-    frames
-  end
-
-  def generate_frame(shot, frame, frames)
-    if frames.size < 10
-      frames << frame.dup if frame.size >= 2 || shot == 10
-    else # last frame
-      frames.last << shot
-    end
-    frames
-  end
-
-  def generate_cal_frame(game, num)
-    frame, next_frame, after_next_frame = game.slice(num, 3)
-    next_frame ||= []
-    after_next_frame ||= []
-    left_shots = next_frame + after_next_frame
-
-    strike_or_spare_point = strike_or_spare(num, frame, left_shots)
-    return strike_or_spare_point unless strike_or_spare_point.nil?
-
-    [frame[0], frame[1]]
-  end
-
-  def strike_or_spare(num, frame, left_shots)
-    if num == 9 # last
-      return [frame[0], frame[1], frame[2]] if frame[0] == 10 || frame[0] + frame[1] == 10 # strike or spare
-    else
-      return [frame[0], left_shots[0], left_shots[1]] if frame[0] == 10 # strike
-      return [frame[0], frame[1], left_shots[0]] if frame.map.sum == 10 # spare
-    end
+  def score
+    frame.sum
   end
 end
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -5,16 +5,41 @@ require './shot'
 require 'byebug'
 
 class Game
-  attr_reader :shots
-
   def initialize(score)
-    scores = score[0].split(',')
-    @shots = scores.map { |s| Shot.new(s).score }
+    @shots = score[0].split(',').map { |shot| Shot.new(shot).score }
   end
 
   def generate_score
-    frames = Frame.new(@shots)
-    point = frames.generate_point_cal_frame
-    point.flatten.sum
+    game = generate_frames
+    game.sum
+  end
+
+  private
+
+  def generate_frames
+    frame = []
+    frame_scores = []
+    @shots.each_with_index do |shot, idx|
+      break if frame_scores.size >= 10
+
+      frame << shot
+      frame = generate_frame(frame, shot, idx)
+
+      if frame.size >= 2 || shot == 10
+        frame_scores << Frame.new(frame).score
+        frame.clear
+      end
+    end
+    frame_scores
+  end
+
+  def generate_frame(frame, shot, idx)
+    if frame.size >= 2
+      frame << @shots[idx + 1] if frame.sum == 10 # spare
+    elsif frame[0] == 10 && shot == 10 # strike
+      frame << @shots[idx + 1]
+      frame << @shots[idx + 2]
+    end
+    frame
   end
 end
