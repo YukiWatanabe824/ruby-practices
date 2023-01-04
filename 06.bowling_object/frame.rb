eae366d7db80dd4775c8f90a547dfb72bbf27bb2
YukiWatanabe824 <gyggg950@gmail.com>
2023-01-04 11:05:41 +0900

    refactor: 真偽判定メソッドの返り値をtrue/falseに修正 / メソッド名修正

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,7 +3,7 @@
 require './shot'
 
 class Frame
-  attr_reader :shots, :num
+  private attr_reader :shots
 
   def initialize(shots, num)
     @shots = shots
@@ -14,7 +14,7 @@ class Frame
     if last_frame?
       base_score
     elsif strike?
-      return base_score + next_frame.two_shot_sum if before_last_frame?
+      return base_score + next_frame.two_shot_sum if nine_frame?
 
       return base_score + next_frame.two_shot_sum + after_next_frame.first_shot if next_frame.strike?
 
@@ -31,7 +31,7 @@ class Frame
   end
 
   def strike?
-    'strike' if shots[0] == 10
+    shots[0] == 10
   end
 
   def two_shot_sum
@@ -39,17 +39,17 @@ class Frame
   end
 
   def spare?
-    'spare' if shots.sum == 10
+    shots.sum == 10
   end
 
   private
 
-  def before_last_frame?
-    'the one before last frame' if num == 8
+  def nine_frame?
+    @num == 8
   end
 
   def last_frame?
-    'last' if num == 9
+    @num == 9
   end
 
   def base_score
